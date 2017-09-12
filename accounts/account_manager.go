2ecec2cc42eca801bebc79cbdc8cbd866462ddce
AlphaSue <409083575@qq.com>
2017-09-12 13:46:45 -0700

    try to debug the CoreCluster

--- accounts/account_manager.go
+++ accounts/account_manager.go
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
+//
 // Package accounts implements encrypted storage of secp256k1 private keys.
 //
 // Keys are stored as encrypted JSON files according to the Web3 Secret Storage specification.
